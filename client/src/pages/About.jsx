01394b7a3a7c04575350d702d2f83a0978d461b6
tulsee <ramtulsi022@gmail.com>
2024-02-04 16:51:46 +0545

    completed about page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,5 +1,21 @@
 const About = () => {
-  return <div>About</div>;
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <div className="max-w2xl mx-auto p-3 text-center">
+        <div className="">
+          <h1 className="text-3xl font font-semibold text-center my-7">
+            About Tulsee&apos;s Blog
+          </h1>
+          <div className="text-md text-gray-500 flex flex-col gap-6">
+            <p>
+              Tulsee&apos;s Blog is a blog that i created to share my thoughts
+              and ideas with the world. I hope you enjoy reading my blog
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default About;
